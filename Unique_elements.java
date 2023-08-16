73ec6317ee6d3d3fe3fa9be78527758b74bde0af
ojesh2000 <ojeshparashar@gmail.com>
2023-08-16 14:11:45 +0530

    find all unique elements in a list

--- Unique_elements.java
+++ Unique_elements.java
@@ -3,7 +3,32 @@ import java.io.*;
 import java.util.concurrent.*;
 
 public class Unique_elements{
+    public static void main(String[] args) throws IOException{
+        //Input Format
+        // n -> No. of elements in the array
+        // next line contains a list of n integers
+        
+        FastIO fio = new FastIO();
+        //Reading Integer
+        Integer n = fio.readInt();
+        //Reading Array
+        ArrayList<Integer> a = fio.readArray();
 
+        TreeSet<Integer> s = new TreeSet<>();
+
+        //Putting elements in a BST(repitions not allowed)
+        for(Integer x : a){
+            s.add(x);
+        }
+        
+        //Printing results
+        System.out.println("The Unique Elements are: ");
+        for(Integer x : s){
+            System.out.print(x);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
 }
 
 
